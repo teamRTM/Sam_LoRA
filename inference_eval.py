2b3c8164ae5a32e0205eec9cc39d75f7b4263ae2
injo kim <deulseong1121@gmail.com>
2023-11-17 13:02:32 +0900

    [Feat] Add AUROC metrcis for SAM LoRA: Prototype (#2)

--- inference_eval.py
+++ inference_eval.py
@@ -11,6 +11,7 @@ import src.utils as utils
 from src.dataloader import DatasetSegmentation, collate_fn
 from src.processor import Samprocessor
 from src.segment_anything import build_sam_vit_b, SamPredictor
+from src.metrics import calculate_auroc_from_logits
 from src.lora import LoRA_sam
 import matplotlib.pyplot as plt
 import yaml
@@ -48,7 +49,9 @@ categroy = anomalib_config_file["dataset"]["category"]
 # Get rank list
 rank_list = config_file["SAM"]["RANKS"]
 rank_loss = []
+rank_auroc = []
 total_baseline_loss = []
+total_baseline_auroc = []
 baseline_loss = 0
 
 # Load SAM model
@@ -68,10 +71,18 @@ with torch.no_grad():
         )  # We need to get the [B, C, H, W] starting from [H, W]
         loss = seg_loss(outputs[0]["low_res_logits"], gt_mask_tensor.float().to(device))
 
+        # Calculate AUROC
+        auroc = calculate_auroc_from_logits(
+            gt_mask_tensor.cpu().numpy(), outputs[0]["low_res_logits"].cpu().numpy()
+        )
+
         total_baseline_loss.append(loss.item())
+        total_baseline_auroc.append(auroc)
 
     print(f"Mean dice score: {mean(total_baseline_loss)}")
+    print(f"Mean AUROC score: {mean(total_baseline_auroc)}")
     baseline_loss = mean(total_baseline_loss)
+    baseline_auroc = mean(total_baseline_auroc)
 
     for rank in rank_list:
         sam = build_sam_vit_b(checkpoint=config_file["SAM"]["CHECKPOINT"])
@@ -96,6 +107,7 @@ with torch.no_grad():
         model.to(device)
 
         total_score = []
+        total_auroc = []
         for i, batch in enumerate(tqdm(test_dataloader)):
             outputs = model(batched_input=batch, multimask_output=False)
 
@@ -106,17 +118,26 @@ with torch.no_grad():
                 outputs[0]["low_res_logits"], gt_mask_tensor.float().to(device)
             )
 
+            # Calculate AUROC
+            auroc = calculate_auroc_from_logits(
+                gt_mask_tensor.cpu().numpy(), outputs[0]["low_res_logits"].cpu().numpy()
+            )
+
             total_score.append(loss.item())
+            total_auroc.append(auroc)
 
         print(f"Mean dice score: {mean(total_score)}")
         rank_loss.append(mean(total_score))
+        rank_auroc.append(mean(total_auroc))
 
 
 print("RANK LOSS :", rank_loss)
+print("RANK AUROC :", rank_auroc)
 
+# ------------------- Plot loss graph ------------------- #
 width = 0.25  # the width of the bars
 multiplier = 0
-models_results = {
+models_loss_results = {
     "Baseline": baseline_loss,
     "Rank 4": rank_loss[0],
     "Rank 8": rank_loss[1],
@@ -127,11 +148,12 @@ models_results = {
     "Rank 512": rank_loss[6],
     "Rank 1024": rank_loss[7],
 }
+
 eval_scores_name = ["Rank"]
 x = np.arange(len(eval_scores_name))
 fig, ax = plt.subplots(layout="constrained")
 
-for model_name, score in models_results.items():
+for model_name, score in models_loss_results.items():
     offset = width * multiplier
     rects = ax.bar(x + offset, score, width, label=model_name)
     ax.bar_label(rects, padding=3)
@@ -144,4 +166,39 @@ ax.set_xticks(x + width, eval_scores_name)
 ax.legend(loc=1, ncols=2)
 ax.set_ylim(0, 0.5)
 
-plt.savefig("../results/final_results/rank_comparison.jpg")
+plt.savefig("../results/final_results/rank_loss_comparison.jpg")
+
+# ------------------- Plot AUROC graph ------------------- #
+
+width = 0.25  # the width of the bars
+multiplier = 0
+models_auroc_results = {
+    "Baseline": baseline_auroc,
+    "Rank 4": rank_auroc[0],
+    "Rank 8": rank_auroc[1],
+    "Rank 16": rank_auroc[2],
+    "Rank 64": rank_auroc[3],
+    "Rank 128": rank_auroc[4],
+    "Rank 256": rank_auroc[5],
+    "Rank 512": rank_auroc[6],
+    "Rank 1024": rank_auroc[7],
+}
+
+eval_scores_name = ["Rank"]
+x = np.arange(len(eval_scores_name))
+fig, ax = plt.subplots(layout="constrained")
+
+for model_name, score in models_loss_results.items():
+    offset = width * multiplier
+    rects = ax.bar(x + offset, score, width, label=model_name)
+    ax.bar_label(rects, padding=3)
+    multiplier += 1
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel("AUROC")
+ax.set_title("LoRA trained on 50 epochs - Rank comparison on test set")
+ax.set_xticks(x + width, eval_scores_name)
+ax.legend(loc=1, ncols=2)
+ax.set_ylim(0, 1)
+
+plt.savefig("../results/final_results/rank_auroc_comparison.jpg")
